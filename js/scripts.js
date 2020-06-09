31beed11894b604c8d217d39da3253b797f24a10
Peter Grimm <pagrimm@gmail.com>
2020-06-09 15:24:07 -0700

    update html, add js logic
    Co-authored-by: Kate Skorija <kateskorija@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,4 @@
-$(document).ready(function() {
-  $("form#leap-year").submit(function(event) {
-    event.preventDefault();
-    const year = parseInt($("input#year").val());
-    const result = leapYear(year);
-    $("#result").text(result);
-  });
-});
-
+// business logic
 function leapYear(year) {
   if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)) {
     return true;
@@ -14,3 +6,22 @@ function leapYear(year) {
     return false;
   }
 }
+
+// user interface logic
+$(document).ready(function() {
+  $("form#leap-year").submit(function(event) {
+    event.preventDefault();
+    const year = parseInt($("input#year").val());
+    const result = leapYear(year);
+
+    $(".year").text(year);
+
+    if (!result) {                 // same as writing if (result === false)
+      $(".not").text("not");
+    } else {
+      $(".not").text("");
+    }
+
+    $("#result").show();
+  });
+});
